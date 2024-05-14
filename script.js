c6413819d717f64acfee6c97b359c3c5a3810b5d
kk0425 <kk0425@gmail.com>
2024-05-14 02:52:28 -0400

    added prototyping

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ function Book(title, author, numOfPages, hasBeenRead) {
   this.author = author;
   this.pages = numOfPages;
   this.hasBeenRead = hasBeenRead ? "Has been read." : "Has not been read.";
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasBeenRead}`;
-  };
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+Book.prototype.info = function() {
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasBeenRead}`;
+}
+
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
 
 console.log(theHobbit.info());
