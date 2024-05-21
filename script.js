3012cd666e120c2653ebe78e46283b3a1d7aa50c
kk0425 <kk0425@gmail.com>
2024-05-21 02:38:40 -0400

    display library to webpage

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const library = [];
+addBook("Star Wars", "George Forman", 5, false);
+addBook("How to be a Bird", "Dee's a Bird!", 420, true);
+
 function Book(title, author, numOfPages, hasBeenRead) {
   this.title = title;
   this.author = author;
@@ -9,6 +13,17 @@ Book.prototype.info = function() {
   return `${this.title} by ${this.author}, ${this.pages} pages, ${this.hasBeenRead}`;
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+function addBook(title, author, numOfPages, hasBeenRead) {
+  library.push(new Book(title, author, numOfPages, hasBeenRead));
+}
 
-console.log(theHobbit.info());
+const libraryContainer = document.querySelector('#library-list');
+function displayLibrary() {
+  library.forEach((e) => {
+    const bookContainer = document.createElement('div');
+    bookContainer.classList.add('book-container');
+    bookContainer.textContent = e.info();
+    libraryContainer.appendChild(bookContainer);
+  });
+}
+displayLibrary();
